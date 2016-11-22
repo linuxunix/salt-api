6674bc2af18016d6ec8ae9eac41d8330692adb1a
linuxunix <13539444219@139.com>
2016-11-22 22:42:46 +0800

    更新salt-api

--- saltstack/views.py
+++ saltstack/views.py
@@ -3,7 +3,7 @@ from saltapi import *
 # Create your views here.
 
 def index(request):
-    minions = SaltAPI().list_all_key()
+    minions_list = SaltAPI().list_all_key()
 
     return render(request, 'SaltStack/salt-api.html', locals())
 
