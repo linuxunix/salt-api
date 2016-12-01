3b48849c9c5bc1f5b2d28cbcc115ae862792badb
linwj <767568971@qq.com>
2016-12-01 19:17:56 +0800

    update salt_deploy

--- saltstack/tests.py
+++ saltstack/tests.py
@@ -1,38 +1,20 @@
-# -*- coding: utf-8 -*-
-from django.test import TestCase
-a='docker,docker1,docker2'
-print a.split(',')
-
-
-
-# -*- coding: utf-8 -*-
-import sys
-import nmap
-scan_row = [ ]
-input_data = raw_input('Please input host and port: ')
-scan_row = input_data.split(" ")
-if len( scan_row ) !=2:
-    print "Input errors,example\"192.168.1.0/24 80,443,22\""
-    sys.exit(0)
-hosts = scan_row[0]  #接收用户输入的主机
-port = scan_row[1]  #接收用户输入的端口
-try:
-    nm = nmap.PortScanner() #创建端口扫描对象
-except nmap.PortScannerError:
-    print ('Nmap not found,',sys.exc_info()[0])
-    sys.exit(0)
-try:
-    nm.scan(hosts=hosts,arguments='-v -sS -p'+port)
-except Exception,e:
-    print "Scan error:"+str(e)
-for host in nm.all_hosts():
-    print('----------------------------------------------------')
-    print('Host : %s (%s)' % (host, nm[host].hostname()))
-    print('State : %s' % nm[host].state())
-    for proto in nm[host].all_protocols():
-        print('----------')
-        print('Protocol : %s' % proto)
-        lport = nm[host][proto].keys()
-        lport.sort()
-        for port in lport:
-            print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
+# #coding=utf8
+# import salt.client # 导入模块
+# client = salt.client.LocalClient() # 初始化 saltstack
+# def overview(request): # 获取所有机器信息
+# target = request.GET.get("target",'*') # 获取请求中的
+# try:
+# grains = client.cmd(target, 'grains.items') #
+# except:
+# grains = {} # 如果出错 ,  将 grains  设置为空
+# return grains # 返回 target
+# def execute(**kwargs): # 执行命令函数
+# return client.cmd_async(**kwargs) # 直接将命令发送到后端
+# def get_state(target): # 获取所有可用的状态
+# try:
+# states = client.cmd(target,'state.show_top') #
+# except:
+# states = {} # 如果失败将状态设置为空
+# return states # 返回状态
+# if __name__ =="__main__": # 如果执行执行 ,  调用下面的命令
+# print get_state('minion1')
